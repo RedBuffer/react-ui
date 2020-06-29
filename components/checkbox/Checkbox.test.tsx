a669ac7bf7f784c864733b7149a02cfc729f3d7b
Shehryar Shoukat <14bscssshoukat@seecs.edu.pk>
2020-06-29 14:16:50 +0500

    add `Checkbox` tests

---
+++ components/checkbox/Checkbox.test.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+
+import Checkbox from './Checkbox';
+
+describe('Checkbox', () => {
+  let handleChange;
+  let wrapper;
+
+  beforeEach(() => {
+    handleChange = jest.fn((e) => e.target.value);
+    wrapper = mount(
+      <Checkbox id={'checkbox'} onChange={handleChange}>
+        checkbox
+      </Checkbox>,
+    );
+  });
+
+  it('should render correctly with menu', () => {
+    const component = wrapper;
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('click on checkbox', () => {
+    wrapper
+      .find('#checkbox')
+      .hostNodes()
+      .simulate('change', { target: { checked: true } });
+
+    expect(handleChange.mock.calls.length).toEqual(1);
+    expect(handleChange.mock.calls[0][0].target.checked).toEqual(true);
+  });
+
+  it('click on disabled checkbox', () => {
+    wrapper.setProps({ disabled: true });
+    wrapper.find('#checkbox').hostNodes().simulate('click');
+
+    expect(handleChange.mock.calls.length).toEqual(0);
+  });
+});
